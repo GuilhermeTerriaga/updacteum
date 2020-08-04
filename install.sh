2cf9ea7972ed6fba6d2813de2c3153cef5844c1e
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-08-04 18:14:59 -0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing updacteum";
+
+sudo chmod +x updacteum;
+
+sudo mv updacteum /usr/bin/;
+
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Unninstalling updacteum";
+
+sudo rm /usr/bin/updacteum;
+
