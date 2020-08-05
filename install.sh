9d09542b1974d4cc61dec46c6c043db12d705af0
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-08-04 22:45:22 -0300

    bug fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,49 @@
 
 echo "Installing updacteum";
 
+FILE=./updacteum
+
+if test -f "$FILE"; then
+	sudo chmod +x updacteum;
+
+
+else
+	touch updacteum
+	echo '#!/bin/bash
+
+echo "Updacteum"
+
+echo "Shutdown after updates?" \[Y\|N\]
+
+read VAR2;
+case "$VAR2" in
+	n|N)
+		sudo apt update
+
+		sudo apt upgrade -y
+
+		sudo snap refresh
+
+		;;
+	y|Y)
+
+		sudo apt update
+
+		sudo apt upgrade -y
+
+		sudo snap refresh
+
+		shutdown -h +1
+		
+		;;
+	*)
+		echo "Invalid"
+		
+		;;
+	esac'>updacteum;
+
 sudo chmod +x updacteum;
+fi
 
 sudo mv updacteum /usr/bin/;
 
